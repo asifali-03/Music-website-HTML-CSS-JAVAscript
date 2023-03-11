bdbb1043f5e85c8c5abf3693ec017ecd9cfeb2a9
asifali-03 <125783556+asifali-03@users.noreply.github.com>
2023-03-11 11:21:55 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+    $FirstName=$_POST["FirstName"]
+    $LastName=$_POST["LastName"]
+    $EmailId=$_POST["EmailId"]
+    $EnterPassword=$_POST["EnterPassword"]
+    $ConfirmPassword=$_POST["ConfirmPassword"]
+?>
