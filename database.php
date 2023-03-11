8743f9a325dd0bfd9e6b9ac08e3616ae7bb426d4
asifali-03 <125783556+asifali-03@users.noreply.github.com>
2023-03-11 11:22:53 +0530

    Delete database.php

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $FirstName=$_POST["FirstName"]
-    $LastName=$_POST["LastName"]
-    $EmailId=$_POST["EmailId"]
-    $EnterPassword=$_POST["EnterPassword"]
-    $ConfirmPassword=$_POST["ConfirmPassword"]
-?>
